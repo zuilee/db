ae802a326632fdfe691b937ee4b86a9263982ff7
zuilee <juilee.katpatal@gmail.com>
2020-09-21 11:53:16 +0530

    Student class added

---
+++ src/main/java/com/db/sdlc/sdlctry/Student.java
@@ -0,0 +1,31 @@
+package com.db.sdlc.sdlctry;
+
+public class Student {
+	
+	private int id;
+	
+	private String name;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
--- src/test/java/com/db/sdlc/sdlctry/SdlcTryApplicationTests.java
+++ src/test/java/com/db/sdlc/sdlctry/SdlcTryApplicationTests.java
@@ -1,5 +1,7 @@
 package com.db.sdlc.sdlctry;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SdlcTryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail();
 	}
 
 }
