7bd964132554e1919f6df34006bc7cf4743e191d
zuilee <juilee.katpatal@gmail.com>
2020-09-21 11:23:14 +0530

    Initial commit

---
+++ src/main/java/com/db/sdlc/sdlctry/SdlcTryApplication.java
@@ -0,0 +1,13 @@
+package com.db.sdlc.sdlctry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlcTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdlcTryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/sdlc/sdlctry/SdlcTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.sdlc.sdlctry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdlcTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
